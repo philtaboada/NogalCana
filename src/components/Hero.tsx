4bc0ee915760d17ec51bab39b73bfa05fda7e5fe
philtaboada <philtaboada1997@gmail.com>
2023-05-16 12:34:01 -0500

    nabvar update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,8 +18,7 @@ const Hero = () => {
               El sabor auténtico de la caña <br /> de azúcar en cada botella.
             </p>
             <a
-            href="#contactanos"
-              type="button"
+              href="#contactanos"
               className="inline-block my-4 rounded bg-[white] px-6 py-2 text-xs font-medium uppercase leading-normal text-[#363636] transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
             >
               Conócenos más
